2e63fdc2856e1b02b2e141ad818f77a2871f3b70
csantos <carlos.hrq.rodrigues@gmail.com>
2024-01-14 20:55:37 -0300

    wip

--- src/main/kotlin/br/com/ufrj/coppetecpagamentos/CoppetecPagamentosApplication.kt
+++ src/main/kotlin/br/com/ufrj/coppetecpagamentos/CoppetecPagamentosApplication.kt
@@ -2,9 +2,11 @@ package br.com.ufrj.coppetecpagamentos
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @EnableScheduling
+@EnableDiscoveryClient
 @SpringBootApplication
 class CoppetecPagamentosApplication
 
