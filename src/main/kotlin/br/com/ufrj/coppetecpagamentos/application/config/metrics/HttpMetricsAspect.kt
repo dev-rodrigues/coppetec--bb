9ea8819a11971c978ee729a7fcec6376a1a17426
csantos <carlos.hrq.rodrigues@gmail.com>
2024-01-14 22:02:25 -0300

    wip

---
+++ src/main/kotlin/br/com/ufrj/coppetecpagamentos/application/config/metrics/HttpMetricsAspect.kt
@@ -0,0 +1,32 @@
+package br.com.ufrj.coppetecpagamentos.application.config.metrics
+
+import io.micrometer.core.instrument.MeterRegistry
+import org.aspectj.lang.JoinPoint
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.annotation.Before
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Component
+
+@Aspect
+@Component
+class HttpMetricsAspect @Autowired constructor(private val meterRegistry: MeterRegistry) {
+
+
+    @Before("execution(* br.com.ufrj.coppetecpagamentos.infrastruscture.http.port.impl.BBPortImpl.autenticar(..))")
+    fun beforeAutenticar(joinPoint: JoinPoint): ResponseEntity<*> {
+        val methodName = joinPoint.signature.name
+
+        try {
+            val result = (joinPoint as ProceedingJoinPoint).proceed()
+
+            meterRegistry.counter("autenticar.requests.success", "method", methodName).increment()
+
+            return result as ResponseEntity<*>
+        } catch (ex: Exception) {
+            meterRegistry.counter("autenticar.requests.failure", "method", methodName).increment()
+            throw ex
+        }
+    }
+}
